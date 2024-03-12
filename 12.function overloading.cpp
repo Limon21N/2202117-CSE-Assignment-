8095fcf60e37a4a359bd40ccbd21315c346a495e
Limon <162408572+Limon21N@users.noreply.github.com>
2024-03-12 11:13:38 +0600

    Create 12.function overloading.cpp
    10
    10.11

---
+++ 12.function overloading.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+#include<iostream>
+using namespace std;
+void sum( int x)
+{
+    cout<< x<< endl;
+}void sum(double x)
+{
+    cout<< x<<endl;
+}
+int main()
+{
+   sum(10);
+   sum(10.11);
+    return 0;
+}
