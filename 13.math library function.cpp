3b29256d485780ad1dce55d238651c2f2db3e992
Limon <162408572+Limon21N@users.noreply.github.com>
2024-03-12 11:16:25 +0600

    Create 13.math library function.cpp
    Square root of 4 is: 2
    Absolute value of 4 is: 4
    Ceiling of 4 is: 4

---
+++ 13.math library function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double number = 4.0;
+
+    cout << "Square root of " << number << " is: " << sqrt(number) << endl;
+    cout << "Absolute value of " << number << " is: " << fabs(number) << endl;
+    cout << "Ceiling of " << number << " is: " << ceil(number) << endl;
+
+    return 0;
+}
